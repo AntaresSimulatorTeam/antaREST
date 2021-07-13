a7096880b034910ed6194c767f8b8616e71e5a2c
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-07-13 09:37:34 +0200

    wip
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- webapp/src/components/StudyView/StudyDataView/MatrixView/index.tsx
+++ webapp/src/components/StudyView/StudyDataView/MatrixView/index.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { createRef, useEffect, useRef, useState } from 'react';
 import { HotTable, HotColumn } from '@handsontable/react';
 import { createStyles, makeStyles } from '@material-ui/core';
+import DataTable from 'frappe-datatable';
 import { MatrixType } from '../../../../common/types';
 import 'handsontable/dist/handsontable.min.css';
+import 'frappe-datatable/dist/frappe-datatable.css';
 
-const useStyles = makeStyles(() => createStyles({
-  root: {
-    width: '100%',
-    height: '100%',
-    overflow: 'auto',
-  },
-}));
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      width: '100%',
+      height: '100%',
+      overflow: 'auto',
+    },
+  }));
 
 interface PropTypes {
   matrix: MatrixType;
@@ -18,7 +21,7 @@ interface PropTypes {
 }
 
 type CellType = Array<number | string | boolean>;
-type ColumnsType = {title: string; readOnly: boolean};
+type ColumnsType = { title: string; readOnly: boolean };
 
 export default function MatrixView(props: PropTypes) {
   // eslint-disable-next-line react/destructuring-assignment
@@ -28,32 +31,28 @@ export default function MatrixView(props: PropTypes) {
   const prependIndex = index.length > 0 && typeof index[0] === 'string';
   const [grid, setGrid] = useState<Array<CellType>>([]);
   const [formatedColumns, setColumns] = useState<Array<ColumnsType>>([]);
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const columnsData: Array<ColumnsType> = (prependIndex ? [{ title: 'Time', readOnly }] : []).concat(
-      columns.map((title) => ({ title: String(title), readOnly })),
-    );
+    if (ref.current) {
+      console.log(grid)
+      // eslint-disable-next-line no-new
+      new DataTable('#datatable', {
+        columns: formatedColumns.map((col) => col.title),
+        data: grid,
+      });
+    }
+  }, [formatedColumns, grid, ref]);
+
+  useEffect(() => {
+    const columnsData: Array<ColumnsType> = (
+      prependIndex ? [{ title: 'Time', readOnly }] : []
+    ).concat(columns.map((title) => ({ title: String(title), readOnly })));
     setColumns(columnsData);
 
-    const tmpData = data.map((row, i) => (
-      prependIndex ? [index[i]].concat(row) : row
-    ));
+    const tmpData = data.map((row, i) => (prependIndex ? [index[i]].concat(row) : row));
     setGrid(tmpData);
   }, [columns, data, index, prependIndex, readOnly]);
 
-  return (
-    <div className={classes.root}>
-      <HotTable
-        data={grid}
-        licenseKey="non-commercial-and-evaluation"
-        width="100%"
-        height="100%"
-      >
-        {
-          formatedColumns.map((column) =>
-            <HotColumn key={column.title} settings={column} />)
-        }
-      </HotTable>
-    </div>
-  );
+  return <div id="datatable" ref={ref} />;
 }
